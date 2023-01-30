95a1239f24a5b32b2dd4f414a94ba6b40cb80c54
StefanThoma <stefan.thoma@roche.com>
2023-01-30 13:28:26 +0000

    "

--- try.R
+++ try.R
@@ -1,1 +1,9 @@
 print("hello world")
+
+install.packages("usethis")
+usethis::use_git_config(user.name = "StefanThoma",
+                        user.email = "stefan.thoma@roche.com")
+
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
